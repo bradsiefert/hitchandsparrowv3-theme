f416ab4f25596f51d0d795c8c748ea9278054145
Brad Siefert <bradsiefert@icloud.com>
2019-06-08 11:26:20 -0500

    Fixing the About Page

--- page-templates/about.php
+++ page-templates/about.php
@@ -1,52 +1,264 @@
 <?php get_header(); /* Template Name: About Page */ ?>
 
-<main>
+<div class="container-fluid">
   <div class="container">
-    <div class="row about">
-      <div class="col-md-7 col-lg-8">
-        <?php get_template_part( 'partials/breadcrumbs'); ?>
-
-        <article>
-          <h2 class="h1">I promise this won’t be in the third person.</h2>
+    <div class="row">
+      <div class="col-md-offset-3 col-md-6 col-sm-offset-1 col-sm-10">
+        <div class="top-preamble">
+          <h1>About Us</h1>
           <p>
-            Hi, my name is Brad and I’m a product designer specializing in user experience, user interface, and front-end
-            development. I’m originally from the surburbs, but I now make my home in Chicago's
-            <a target="_new "href="https://en.wikipedia.org/wiki/Garfield_Park_(Chicago)">Garfield Park neighborhood</a>
-            with my wife Nicole.
+            Hitch & Sparrow is a wedding photography company based in <strong>Denver, CO</strong> & 
+            <strong>Chicago, IL</strong>. We are deeply passionate about marriage and would love to see the health of 
+            your friendship favor the production of one single day. We are available <em>(and love!)</em> to travel 
+            all over the world to create beautiful images to celebrate  alongside you at the beginning of your marriage.
           </p>
-
-          <p>
-            I love typography, grids, Sketch, Bootstrap,
-            <a target="_new" href="https://www.flickr.com/photos/dim7chord/albums/72157643011912493">instant film</a>,
-            indie software, crossing off tasks from my <a target="_new" href="https://culturedcode.com/">task list</a>,
-            yoga, <a target="_new" href="https://www.patagonia.com/product/mens-nano-air-hoody/84365.html">Patagonia
-            jackets</a>, and most Apple products. I value clarity, elegance, efficiency, and detail orientedness in
-            design. I want to work with people who inspire me to be better each day and who are serious about creating
-            beautiful and meaningful work.
-          </p>
-
-          <p>
-            My favorite places in the world are
-            <a target="_new" href="https://www.flickr.com/photos/dim7chord/albums/72157690018094916">Grand Teton National
-            Park</a> and <a target="_new" href="https://www.flickr.com/photos/dim7chord/sets/72157690018094916">Rabbit
-            Island</a>, there are dozens of documentaries in my Netflix queue, and I’ve taken around 500K photos in the
-            past 11 years.
-          </p>
-
-          <p>
-            I’m slightly addicted to Diet Coke—but I’m working on it.
-          </p>
-          <a target="_blank" class="btn btn-primary mb-32" href="<?= get_theme_file_uri("Brad-Siefert-Resume.pdf") ?>">
-            Download Resume
-          </a>
-        </article>
-      </div>
-
-      <div class="col-md-5 col-lg-4">
-        <img class="img-fluid" src="<?= get_theme_file_uri("images/photo-bradsiefert.jpg") ?>" alt="Photo of Brad Siefert">
+        </div>
       </div>
     </div>
   </div>
-</main>
+</div>
+
+<div class="container-fluid main-content">
+  <div class="container">
+    
+    <div class="row">
+      <div class="col-sm-12">
+        <h2>Who We Are</h2>
+      </div>
+      
+      <div class="col-sm-12 about-us">
+        <div class="row">
+          <div class="col-sm-4">
+            <img src="<?= get_theme_file_uri("images/photo-anthony.jpg") ?>" alt="Photo of Anthony Barlich"/>
+            <div class="name">Anthony</div>
+            <ul class="list-inline mb30">
+              <li>
+                <a target="_new" href="https://www.instagram.com/abarlich/">
+                  <img src="<?= get_theme_file_uri("images/social-instagram-small.svg") ?>" alt="Instagram icon"/>
+                </a>
+              </li>
+              <li>
+                <a target="_new" href="https://www.facebook.com/abarlich">
+                  <img src="<?= get_theme_file_uri("images/social-facebook-small.svg") ?>" alt="Facebook icon"/>
+                </a>
+              </li>
+              <li>
+                <a target="_new" href="http://www.anthonybarlich.com/">
+                  <img src="<?= get_theme_file_uri("images/social-website-small.svg") ?>" alt="Personal Website icon"/>
+                </a>
+              </li>
+            </ul>
+            <p>
+              <strong>Hometown:</strong>Pittsburgh, PA
+            </p>
+            <p class="mb30">
+              <strong>College:</strong>Penn State University
+            </p>
+            <div class="collapse bio" id="collapseAnthonyBio">
+              <p>I'm Anthony. I was born and raised in Pittsburgh, PA. I absolutely love shared experiences and 
+                exploring new places. A few other things I dig: good design, a meal shared with friends, finding 
+                tasty cortado’s in every city, Pittsburgh sports, running/biking with good music or a solid podcast 
+                playing, the outdoors (hiking, rock-climbing, surfing, throwing/hitting/kicking objects back & 
+                fourth, etc.), mid-century modern homes/furniture, a good cocktail (Toronto’s are my favorite), solid 
+                beer (bourbon barrel-aged, please), good candles, camp-side conversations, friendships that extend 
+                across time & state-lines and my beautiful wife, Laura.
+              </p>
+              
+              <p>
+                Photography has given my interests in exploration, storytelling and connecting, a fascinating 
+                platform. I'm insanely passionate about what I do and the relationships I get to build in the 
+                process. Photographing a wedding with Hitch & Sparrow is a stunning example of this reality. It's a 
+                joy to celebrate the marriage process with couples, as I truly see the marriage as more important 
+                than the wedding day.
+              </p>
+              
+              <p>
+                Laura and I recently moved back to Chicago from Denver, CO and we're excited to be back home and grow 
+                H&S. It’s a wild adventure we’re on. Please connect with me AND us (H&S) on instagram or facebook—
+                we're excited to share the journey!
+              </p>
+            </div>
+            
+            <button class="btn btn-info btn-sm mb30" type="button" data-toggle="collapse" data-target="#collapseAnthonyBio" aria-expanded="false" aria-controls="collapseAnthonyBio">
+              Read Bio
+            </button>
+          </div>
+          
+          <div class="col-sm-4">
+            <img src="<?= get_theme_file_uri("images/photo-laura.jpg") ?>" alt="Photo of Laura Barlich"/>
+            <div class="name">Laura</div>
+            <ul class="list-inline mb30">
+              <li>
+                <a target="_new" href="https://www.instagram.com/la_barlich/">
+                  <img src="<?= get_theme_file_uri("images/social-instagram-small.svg") ?>" alt="Instagram icon"/>
+                </a>
+              </li>
+              <li>
+                <a target="_new" href="https://www.facebook.com/laurabarlich">
+                  <img src="<?= get_theme_file_uri("images/social-facebook-small.svg") ?>" alt="Facebook icon"/>
+                </a>
+              </li>
+            </ul>
+            <p>
+              <strong>Hometown:</strong> Elgin, IL
+            </p>
+            <p class="mb30">
+              <strong>College:</strong>  Illinois State University
+            </p>
+            <div class="collapse bio" id="collapseLauraBio">
+              <p>
+                My name is Laura Barlich. I was born and raised in Elgin, IL a suburb of Chicago. At an early age I fell 
+                in love with creating art, making music, playing sports and telling stories. Photography revealed to be 
+                a beautiful and creative outlet to share experiences in an impactful way.
+              </p>
+              
+              <p>
+                After graduating from Illinois State University in 2009, I pursued a career in Art Education and taught 
+                High School Art within Chicago Public Schools until 2014. During my time teaching and coaching soccer, I 
+                met my husband, Anthony Barlich, which reinvigorated my love for photography. After years of balancing 
+                photography, music, teaching and a deep desire to be an artist myself, we decided I’d come into a 
+                permanent role with H&S. In addition, I could pursue a few other passions of mine: most notably, music 
+                and ceramics.
+              </p>
+              
+              <p>
+                I officially function as an assistant photographer for Hitch & Sparrow and have been steadily developing 
+                and growing my ceramics brand, Neubloom, for the past 2 years. It’s been quite the adventure with so 
+                many lessons learned, experiences shared and dreams realized. In our latest adventure, we recently moved 
+                to Denver, CO where we will enjoy heavy doses of nature, hiking, soccer, sunshine and endless 
+                photographic moments. Feel free to follow my new adventures on instagram, etsy and Facebook!
+              </p>
+            </div>
+            
+            <button class="btn btn-info btn-sm mb30" type="button" data-toggle="collapse" data-target="#collapseLauraBio" aria-expanded="false" aria-controls="collapseLauraBio">
+              Read Bio
+            </button>
+          </div>
+          
+          <div class="col-sm-4">
+            <img src="<?= get_theme_file_uri("images/photo-brad.jpg") ?>" alt="Photo of Bradley Siefert"/>
+            <div class="name">Brad</div>
+            <ul class="list-inline mb30">
+              <li>
+                <a target="_new" href="https://www.instagram.com/bradsiefert/">
+                  <img src="<?= get_theme_file_uri("images/social-instagram-small.svg") ?>" alt="Instagram icon"/>
+                </a>
+              </li>
+              <li>
+                <a target="_new" href="https://www.facebook.com/bradleyksiefert">
+                  <img src="<?= get_theme_file_uri("images/social-facebook-small.svg") ?>" alt="Facebook icon"/>
+                </a>
+              </li>
+              <li>
+                <a target="_new" href="http://brad.si">
+                  <img src="<?= get_theme_file_uri("images/social-website-small.svg") ?>" alt="Personal Website icon"/>
+                </a>
+              </li>
+            </ul>
+            <p>
+              <strong>Hometown:</strong> Crystal Lake, IL
+            </p>
+            <p class="mb30">
+              <strong>College:</strong>  Illinois State University
+            </p>
+            
+            <div class="collapse bio" id="collapseBradBio">
+              <p>
+                Hey, I'm Brad, I’m a Chicagoland native who came to photography after beginning a career as a graphic 
+                designer for startups and web companies. I love the technical end of photography: lenses, lights, 
+                strobes, triggers, etc. Knowing these concepts has helped me to think creatively even in the midst of a 
+                hectic wedding day. I love the run-and-gun challenge that a wedding presents, but my favorite parts are 
+                the sweet moments throughout a day where I get to experience the joy of a loving couple.
+              </p>
+              
+              <p>
+                I’m enamored with beautiful landscapes, captivating portraits, and simple moments between people. On a 
+                wedding day, I get to fly into a bride and groom’s world and to capture something they will cherish for 
+                the rest of their lives. It’s a challenge and an honor that I don’t take lightly. I’ve done it enough to 
+                not get nervous anymore, but I’m still excited and motivated to create something that the couple will be 
+                moved by and will love forever.
+              </p>
+            </div>
+            
+            <button class="btn btn-info btn-sm mb30" type="button" data-toggle="collapse" data-target="#collapseBradBio" aria-expanded="false" aria-controls="collapseBradBio">
+              Read Bio
+            </button>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <?php get_template_part( 'partials/featured-on'); ?>
+    
+    <div class="row">
+      <div class="col-sm-12">
+        <h2>Reviews</h2>
+      </div>
+      
+      <div class="col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10 featured-on mb30">
+        <p class="mb30">
+          We love that people have said so many kind things about our work. We always strive to create something 
+          beautiful and meaningful on your day.
+        </p>
+        
+        <ul class="list-inline reviewed mt40">
+          <li>
+            <a target="_new" href="http://www.yelp.com/biz/hitch-and-sparrow-wedding-co-chicago-2">
+              <img src="<?= get_theme_file_uri("images/reviewed-yelp.png") ?>" alt="Our reviews on Yelp"/>
+            </a>
+          </li>
+          <li>
+            <a target="_new" href="http://www.weddingwire.com/biz/hitch-sparrow-wedding-photography-chicago/65f4d10c1068848e.html">
+              <img src="<?= get_theme_file_uri("images/reviewed-weddingwire.png") ?>" alt="Our reviews on Wedding Wire"/>
+            </a>
+          </li>
+          <li>
+            <a target="_new" href="https://www.theknot.com/marketplace/hitch-and-sparrow-wedding-co-chicago-il-648468">
+              <img src="<?= get_theme_file_uri("images/reviewed-theknot.png") ?>" alt="Our reviews on The Know"/>
+            </a>
+          </li>
+        </ul>
+      </div>
+      
+      <div class="col-sm-12">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="client-testimonial">
+              "Hitch & Sparrow was a terrific match for us from the start. Anthony met with us to talk about their approach and answer all our questions. I initially contacted them because their portfolio is gorgeous and just our style (not cliche and cookie-cutter like a lot of wedding photographers). What sealed the deal was how easy-going and likeable Anthony is."
+              <div class="testimonial-author">AJ, Chicago, IL</div>
+            </div>
+          </div>
+          
+          <div class="col-sm-6">
+            <div class="client-testimonial">
+              "We wanted a photographer who would take not only the typical posed family photos but also a lot of candid pictures of the ceremony, the reception, the guests, the garden venue, and us. H&S did all that, and more, finding the best backdrops and the most gorgeous light and setting up both fun and more romantic poses."
+              <div class="testimonial-author">Danica, Brooklyn, NY</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="col-sm-12">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="client-testimonial">
+              "I can't get over how stunning our wedding photos are. They captured such intimate moments that we'll have forever. We got married at the Nature Museum and we had horrible weather that day. I had it in my head that we would have our first look photos…by the lake and in Lincoln Park, and was really worried that we wouldn't have good photos because of the rain. I couldn't have been more wrong."
+              <div class="testimonial-author">Emily, Chicago, IL</div>
+            </div>
+          </div>
+          
+          <div class="col-sm-6">
+            <div class="client-testimonial">
+              "The pictures they took of our wedding are incredible. They are true artists! Photojournalistic, technically gorgeous, and with real-life, classic post-production. H&S capture the emotion, moments, details, and moments that you will treasure for the rest of your life. Truly masters of their craft, you will have timeless art that is worth the investment. We couldn't have had nicer people alongside us and capturing our big day."
+              <div class="testimonial-author">Kathryn, Yulee, FL</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</div>
 
 <?php get_footer(); ?>
