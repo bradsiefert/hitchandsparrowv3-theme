0cdcf53387b4271e15769c78738c2a5b81f0cc6a
Brad Siefert <bradsiefert@icloud.com>
2019-06-08 22:05:02 -0500

    Working on blogroll WP loop stuff

--- page-templates/blog.php
+++ page-templates/blog.php
@@ -4,24 +4,33 @@
 <div class="blog-contain">
   <h1 class="hidden">Blog</h1>
   
-  <div class="blog-featured">
-    <div class="blog-featured-details">
-      <div class="blog-post-featured-image">
-        <a href="">
-          <img src="http://hitchandsparrow.com/storage/app/uploads/public/5cf/7e3/f7c/5cf7e3f7cbfee527653980.jpg">
-        </a>
+  
+    <?php $wp_query->query('posts_per_page=15&order=DESC'.'&paged='.$paged); while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+      <div class="blog-featured">
+        <div class="blog-featured-details">
+          <div class="blog-post-featured-image">
+            <a href="">
+              <?php the_post_thumbnail('medium', array('class' => 'img-responsive')); ?>
+              <!-- <?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive responsive--full', 'title' => 'Feature image']); ?> -->
+            </a>
+          </div>
+          
+          <div class="blog-title">
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+          </div>
+          <div class="blog-date-location">
+            Posted on: Jun 05, 2019
+          </div>
+          
+          <a href="http://hitchandsparrow.com/blog/weddings/tyler-and-emily/" class="btn btn-primary btn-sm">View Post</a>
+        </div>
       </div>
-      
-      <div class="blog-title">
-        <a href="http://hitchandsparrow.com/blog/weddings/tyler-and-emily/">Tyler &amp; Emily</a>
-      </div>
-      <div class="blog-date-location">
-        Posted on: Jun 05, 2019
-      </div>
-      
-      <a href="http://hitchandsparrow.com/blog/weddings/tyler-and-emily/" class="btn btn-primary btn-sm">View Post</a>
-    </div>
-  </div>
+    
+    <?php endwhile; ?>
+  
+  
+  
+  
   
   <div class="archive-tile">
     <p>To see more of our work, please check out our Archives.</p>
