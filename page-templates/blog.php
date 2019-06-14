a8c67b0aa4954180f5b07b96e01bf58129fbc941
Brad Siefert <bradsiefert@icloud.com>
2019-06-13 21:39:08 -0500

    Cleaning up the blog list page

--- page-templates/blog.php
+++ page-templates/blog.php
@@ -4,42 +4,50 @@
 <div class="blog-contain">
   <h1 class="hidden">Blog</h1>
   
-  
-    <?php $wp_query->query('posts_per_page=15&order=DESC'.'&paged='.$paged); while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-      <div class="blog-featured">
-        <div class="blog-featured-details">
-          <div class="blog-post-featured-image">
-            <a href="">
-              <?php the_post_thumbnail('medium', array('class' => 'img-responsive')); ?>
-              <!-- <?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive responsive--full', 'title' => 'Feature image']); ?> -->
-            </a>
-          </div>
-          
-          <div class="blog-title">
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-          </div>
-          <div class="blog-date-location">
-            Posted on: Jun 05, 2019
-          </div>
-          
-          <a href="http://hitchandsparrow.com/blog/weddings/tyler-and-emily/" class="btn btn-primary btn-sm">View Post</a>
+  <?php
+    $wp_query->query('posts_per_page=7&order=DESC'.'&paged='.$paged);
+    while ($wp_query->have_posts()) :
+    $wp_query->the_post(); 
+  ?>
+    <div class="blog-featured">
+      <div class="blog-featured-details">
+        <div class="blog-post-featured-image">
+          <a href="<?php the_permalink(); ?>">
+            <?php the_post_thumbnail('medium', array('class' => 'img-responsive')); ?>
+          </a>
         </div>
+
+        <div class="blog-title">
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </div>
+        <div class="blog-date-location">
+          Posted on: Jun 05, 2019
+        </div>
+        
+        <a href="<?php the_permalink(); ?>" class="btn btn-primary btn-sm">View Post</a>
       </div>
-    
-    <?php endwhile; ?>
-  
-  
-  
-  
-  
-  <div class="archive-tile">
-    <p>To see more of our work, please check out our Archives.</p>
-    <ul class="pagination">
-      <li class="active">
-        <button class="btn btn-primary btn-sm" href="/blog?page=2">More Posts</button>
-      </li>
-     </ul>
-  </div>
+    </div>
+  <?php endwhile; ?>
+
+  <?php if ($paged > 1) { ?>
+    <div class="archive-tile">
+      <p>No more blog posts to show.</p>
+      <ul class="pagination">
+        <li class="active">
+          <a class="btn btn-primary btn-sm" href="/blog">Go back home</a>
+        </li>
+      </ul>
+    </div>
+  <?php } else { ?>
+    <div class="archive-tile">
+      <p>To see more of our work, please check out our Archives.</p>
+      <ul class="pagination">
+        <li class="active">
+          <?php next_posts_link('More Posts >'); ?>
+        </li>
+      </ul>
+    </div>
+  <?php } ?>
 </div>
 
 <?php get_template_part( 'partials/footer-blog'); ?>
--- partials/footer-blog.php
+++ partials/footer-blog.php
@@ -1,5 +1,6 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
+<script src="<?= get_theme_file_uri("js/jquery.min.js") ?>"></script>
 <script src="<?= get_theme_file_uri("js/bootstrap.min.js") ?>"></script>
 <script src="<?= get_theme_file_uri("js/retina.earlybird.min.js") ?>"></script>
 
