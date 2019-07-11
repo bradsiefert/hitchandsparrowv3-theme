682c7db56f4bba8ea9eafa84793da0927bc0d6d4
Brad Siefert <bradsiefert@icloud.com>
2019-07-10 19:51:34 -0500

    Wiring up booking page

--- page-templates/book-us.php
+++ page-templates/book-us.php
@@ -36,74 +36,11 @@
         <div class="confirm-container">
           <div class="row">
             <div class="col-sm-12">
-              <form role="form" data-request="onContactSent" data-request-update="'confirm': '.confirm-container'" data-request-success="$('#form-contact').slideUp(1000)" id="form-contact">
-                
-                <div class="form-group">
-                  <input type="text" name="name" class="form-control" id="nameForm" placeholder="Name">
-                </div>
-                
-                <div class="form-group">
-                  <input type="text" name="email" class="form-control" id="emailForm" placeholder="Email Address">
-                </div>
-                
-                <div class="form-group">
-                  <input type="text" name="location" class="form-control" id="locationForm" placeholder="Wedding Location (and venue, if known)">
-                </div>
-                
-                <div class="form-group">
-                  <input type="text" name="weddingdate" class="form-control" id="weddingdateForm" placeholder="Wedding Date (if known)">
-                </div>
-                
-                <div class="row">
-                  <div class="col-sm-6">
-                    <div class="form-group">
-                      <select class="form-control" name="budget" id="budgetForm">
-                        <option value="Unanswered">Photography Budget</option>
-                        <option value="$3500 and under">$3500 and under</option>
-                        <option value="$3500 to $5000">$3500 to $5000</option>
-                        <option value="$5000 and up">$5000 and up</option>
-                      </select>
-                    </div>
-                  </div>
-                  
-                  <div class="col-sm-6">
-                    <div class="form-group">
-                      <input class="form-control" type="text" id="findusForm" name="findus" value="" placeholder="How did you find us?">
-                    </div>
-                  </div>
-                  
-                </div>
-                
-                <div class="form-group">
-                  <input class="form-control" type="text" id="messageForm" name="body" value="" placeholder="Anything else we should know?">
-                </div>
-                
-                <div class="form-group">
-                  <label>I'm interested in photos for:</label><br />
-                  <label class="checkbox-inline">
-                    <input class="booking-check" type='checkbox' id="CheckboxWeddingPhotos" name="CheckboxWeddingPhotos"/><label for="CheckboxWeddingPhotos"></label>
-                    <div class="form-checkbox-label">Wedding</div>
-                  </label>
-                  
-                  <label class="checkbox-inline">
-                    <input class="booking-check" type='checkbox' id="CheckboxEngagementPhotos" name="CheckboxEngagementPhotos"/><label for="CheckboxEngagementPhotos"></label>
-                    <div class="form-checkbox-label">Engagement</div>
-                  </label>
-                  
-                  <label class="checkbox-inline">
-                    <input class="booking-check" type='checkbox' id="CheckboxPhotoboothPhotos" name="CheckboxPhotoboothPhotos"/><label for="CheckboxPhotoboothPhotos"></label>
-                    <div class="form-checkbox-label">Photobooth</div>
-                  </label>
-                </div>
-                
-                <div class="row">
-                  <div class="col-sm-12">
-                    <button type="submit" class="btn btn-default btn-lg btn-block mb30">Send Inquiry</button>
-                  </div>
-                </div>
-                
-              </div>
-            </div>
+              <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); the_content(); endwhile; else: ?>
+              <p>Sorry, no posts matched your criteria.</p>
+              <?php endif; ?>
+
+            </div></div>
           </form>
         </div>
       </div>
